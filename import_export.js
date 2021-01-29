1223c50e735225cc8348392f925f33905dfaf570
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2021-01-29 12:26:26 +0100

    undo prova import-export

--- import_export.js
+++
@@ -1,9 +0,0 @@
-import {lang} from "./lang.js"
-
-document.querySelectorAll(".lang").forEach((button) => {
-    button.addEventListener("click", () => {
-        lang(button.value);  // executa la funció per canviar de llenguatge
-    })
-})
-
-lang("cat");  // carrega el primer llenguatge (es pot evitar posant el llenguatge per defecte a l'HTML) 
